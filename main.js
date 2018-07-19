a9b3b648aa4a6077c5976046756ad017c5f23727
krishnasrichalla12 <krishnasri.challa12@gmail.com>
2018-07-19 12:17:35 +0530

    commit all

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
-function getfile(file,callback)
+// function getfile(file,callback)
+// {
+//   var xhr=new XMLHttpRequest();
+//   xhr.overrideMimeType("application/json");
+//   xhr.open("GET",file,true);
+//   xhr.onreadystatechange=function(){
+//     if(xhr.readyState === 4 && xhr.status=="200"){
+//     callback(xhr.responseText);
+//   }
+//   };
+// xhr.send(null);
+// }
+// getfile("data.json",function(text){
+//   var data=JSON.parse(text);
+//   console.log(data);
+//   details(data.basics);
+//   career(data.CareerObjective);
+//   education(data.education);
+//   technicalskills(data.Skills);
+//   achieve(data.Achievements);
+// })
+function loadjson(file)
 {
-  var xhr=new XMLHttpRequest();
-  xhr.overrideMimeType("application/json");
-  xhr.open("GET",file,true);
-  xhr.onreadystatechange=function(){
-    if(xhr.readyState === 4 && xhr.status=="200"){
-    callback(xhr.responseText);
+  return new Promise((resolve,reject)=>{
+    return fetch(file).then(response=>{
+      if(response.ok){
+        resolve(response.json());
+      }
+      else{
+        reject(new Error('error'))
+      }
+    })
+    })
   }
-  };
-xhr.send(null);
-}
-getfile("data.json",function(text){
-  var data=JSON.parse(text);
-  console.log(data);
+  var newfile = loadjson("data.json")
+  newfile.then(data=>{
   details(data.basics);
   career(data.CareerObjective);
+  education(data.education);
+  technicalskills(data.Skills);
+  achieve(data.Achievements);
 })
+
 var child=document.querySelector(".childone");
 function details(det)
 {
@@ -45,7 +70,7 @@ function details(det)
 var child2=document.querySelector(".childtwo");
 function career(careerinfo)
 {
-  var cr1=document.createElement("h3");
+  var cr1=document.createElement("h2");
   cr1.textContent="CourseObjective:";
   child2.appendChild(cr1);
   var line1=document.createElement("hr");
@@ -53,4 +78,73 @@ function career(careerinfo)
   var cr2=document.createElement("p");
   cr2.textContent=careerinfo.info;
   child2.appendChild(cr2);
+}
+function education(educationinfo){
+  var ed1=document.createElement("h2");
+  ed1.textContent="Educational Qualification";
+  child2.appendChild(ed1);
+  var line2=document.createElement("hr");
+  child2.appendChild(line2);
+  for(i=0;i<educationinfo.length;i++)
+  {
+    var deg=document.createElement("h4");
+    deg.textContent = educationinfo[i].degree;
+    child2.appendChild(deg);
+
+    var edu1=document.createElement("ul");
+    var edu2=document.createElement("li");
+    edu2.textContent=educationinfo[i].institute;
+    edu1.appendChild(edu2);
+    child2.appendChild(edu1);
+
+    var edu4=document.createElement("ul");
+    var edu3=document.createElement("li");
+    edu3.textContent=educationinfo[i].data;
+    edu4.appendChild(edu3);
+    child2.appendChild(edu4);
+  }
+}
+function technicalskills(tech)
+{
+  var tech1=document.createElement("h2");
+  tech1.textContent="Technical Skills";
+  child2.appendChild(tech1);
+  var line3=document.createElement("hr");
+  child2.appendChild(line3);
+  var skilldata=document.createElement("table");
+  skilldata.border="5";
+  child2.appendChild(skilldata);
+  tabledata="";
+  for(i=0;i<tech.length;i++){
+  tabledata+="<tr><td>"+tech[i].title+"</td><td>"+tech[i].data+"</td></tr>"
+}
+skilldata.innerHTML=tabledata;
+  // for(i=0;i<tech.length;i++)
+  // {
+  //   var degr=document.createElement("h3");
+  //   degr.textContent=tech[i].title;
+  //   child2.appendChild(degr);
+  //   var tech2=document.createElement("ul");
+  //   var tech3=document.createElement("li");
+  //   tech3.textContent=tech[i].data;
+  //   tech2.appendChild(tech3);
+  //   child2.appendChild(tech2);
+  // }
+}
+function achieve(ac)
+{
+  var ac1=document.createElement("h2");
+  ac1.textContent="Achievements";
+  child2.appendChild(ac1);
+  var line4=document.createElement("hr");
+  child2.appendChild(line4);
+  for(i=0;i<ac.length;i++)
+  {
+    var edu1=document.createElement("ul");
+    var edu2=document.createElement("li");
+    edu2.textContent=ac[i].inform;
+    edu1.appendChild(edu2);
+    child2.appendChild(edu1);
+  }
+
 }
